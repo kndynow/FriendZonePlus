df0cd581fe543e11b5d0256ce39ecd0e8adb2fcc
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-11-28 16:35:03 +0200

    Created skeleton of register form

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,17 +1,41 @@
-import { Button, Form, Row, Container } from "react-bootstrap";
+import { Button, Form, Card, Row } from "react-bootstrap";
+
+async function handleRegister(event: React.FormEvent) {
+  event.preventDefault();
+}
 
 export default function RegisterPage() {
   return (
-    <Container>
-      <Row>
-        <Form>
+    <Card className="mt-5 mx-2">
+      <Card.Body>
+        <Card.Title className="text-center">Register</Card.Title>
+
+        <Form onSubmit={handleRegister}>
           <Form.Group>
             <Form.Label>Email</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
-            <Button variant="primary">Primary</Button>
           </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>First name</Form.Label>
+            <Form.Control type="text" placeholder="Enter first name" />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>First name</Form.Label>
+            <Form.Control type="text" placeholder="Enter last name" />
+          </Form.Group>
+
+          <Button className="mt-2" variant="primary">
+            Register
+          </Button>
         </Form>
-      </Row>
-    </Container>
+      </Card.Body>
+    </Card>
   );
 }
