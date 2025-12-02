14c81f1f13731e53ce64587a4c9142f1232a1cf4
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-12-02 19:02:49 +0200

    Created hooks-folder which contains empty hook useAuth. Implemented form state management with setForm and setProperty for the RegisterPage.

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,10 +1,29 @@
-import { Button, Form, Card, Row } from "react-bootstrap";
+import { Button, Form, Card } from "react-bootstrap";
+import { useState } from "react";
 
-async function handleRegister(event: React.FormEvent) {
-  event.preventDefault();
-}
+const initialForm = {
+  email: "",
+  password: "",
+  firstName: "",
+  lastName: "",
+};
 
 export default function RegisterPage() {
+  const [form, setForm] = useState(initialForm);
+
+  async function handleRegister(event: React.FormEvent) {
+    event.preventDefault();
+  }
+
+  function setProperty(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
   return (
     <Card className="mt-5 mx-2">
       <Card.Body>
@@ -13,22 +32,50 @@ export default function RegisterPage() {
         <Form onSubmit={handleRegister}>
           <Form.Group>
             <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="email"
+              name="email"
+              value={form.email}
+              placeholder="Enter email"
+              onChange={setProperty}
+              required
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              name="password"
+              value={form.password}
+              placeholder="Password"
+              onChange={setProperty}
+              required
+            />
           </Form.Group>
 
           <Form.Group>
             <Form.Label>First name</Form.Label>
-            <Form.Control type="text" placeholder="Enter first name" />
+            <Form.Control
+              type="text"
+              name="firstName"
+              value={form.firstName}
+              placeholder="Enter first name"
+              onChange={setProperty}
+              required
+            />
           </Form.Group>
 
           <Form.Group>
             <Form.Label>Last name</Form.Label>
-            <Form.Control type="text" placeholder="Enter last name" />
+            <Form.Control
+              type="text"
+              name="lastName"
+              value={form.lastName}
+              placeholder="Enter last name"
+              onChange={setProperty}
+              required
+            />
           </Form.Group>
 
           <Button className="mt-2" variant="primary">
