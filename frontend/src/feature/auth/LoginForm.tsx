d76f28afe94d9591d221ed2255dbaef3bcec920a
Viktoria <viktoria.pakhrova@outlook.com>
2025-12-08 16:36:49 +0100

    Added FormField to LoginForm and moved logic from login page to loginform instead

--- frontend/src/feature/auth/LoginForm.tsx
+++ frontend/src/feature/auth/LoginForm.tsx
@@ -1,20 +1,78 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import { useAuth } from "../../../context/AuthProvider";
+import { Form, useNavigate } from "react-router-dom";
+import FormField from "../../components/ui/FormField";
+import toast from "react-hot-toast";
+
 interface LoginFormProps {
   onSwitchToRegister: () => void;
 }
 
 export default function LoginForm({ onSwitchToRegister }: LoginFormProps) {
+  const navigate = useNavigate();
+  const { login } = useAuth();
+  const [form, setForm] = useState({ usernameOrEmail: "", password: "" });
+  const [submitting, setSubmitting] = useState(false);
+
+  function setProperty(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  }
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    try {
+      setSubmitting(true);
+      await login({
+        usernameOrEmail: form.usernameOrEmail,
+        password: form.password,
+      });
+      toast.success("Log in successful!");
+      navigate("/");
+    } catch (err: any) {
+      const message = (err && (err.message || err.detail)) || "Login failed";
+      toast.error(message);
+    } finally {
+      setSubmitting(false);
+    }
+  }
+
   return (
     <>
-      <h2>Logga in form ska vara här</h2>
-      <p className="text-center mt-3">
-        Inget konto?
-        <button
-          className="text-primary"
-          style={{ cursor: "pointer" }}
-          onClick={onSwitchToRegister}
+      <Form onSubmit={handleSubmit}>
+        <FormField
+          label="Username or Email"
+          placeholder="Username or email"
+          name="usernameOrEmail"
+          value={form.usernameOrEmail}
+          onChange={setProperty}
+        />
+
+        <FormField
+          label="Password"
+          placeholder="Enter password"
+          type="password"
+          name="password"
+          value={form.password}
+          onChange={setProperty}
+        />
+
+        <Button
+          className="mt-2 w-100"
+          variant="primary"
+          disabled={submitting}
+          type="submit"
         >
-          Registrera här
-        </button>
+          {submitting ? "Logging in..." : "Log in"}
+        </Button>
+      </Form>
+
+      <p className="text-center mt-3 px-2">
+        No account yet?
+        <span className="text-primary px-2" onClick={onSwitchToRegister}>
+          Register here!
+        </span>
       </p>
     </>
   );
