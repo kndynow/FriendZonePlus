706efe1371520f35569095b2c9a7044d71c353bc
Viktoria <viktoria.pakhrova@outlook.com>
2025-12-08 21:59:14 +0100

    Extracted logic from loginForm to useLoginForm hook instead for cleaner structure

--- frontend/src/feature/auth/LoginForm.tsx
+++ frontend/src/feature/auth/LoginForm.tsx
@@ -1,19 +1,13 @@
-import { useState } from "react";
 import { Button } from "react-bootstrap";
-import { useAuth } from "../../../context/AuthProvider";
-import { Form, useNavigate } from "react-router-dom";
+import { useLoginForm } from "./useLoginForm";
 import FormField from "../../components/ui/FormField";
-import toast from "react-hot-toast";
 
 interface LoginFormProps {
   onSwitchToRegister: () => void;
 }
 
 export default function LoginForm({ onSwitchToRegister }: LoginFormProps) {
-  const navigate = useNavigate();
-  const { login } = useAuth();
-  const [form, setForm] = useState({ usernameOrEmail: "", password: "" });
-  const [submitting, setSubmitting] = useState(false);
+  const { loginForm, submitting, setProperty, submitLogin } = useLoginForm();
 
   const fields = [
     {
@@ -30,33 +24,10 @@ export default function LoginForm({ onSwitchToRegister }: LoginFormProps) {
     },
   ] as const;
 
-  function setProperty(e: React.ChangeEvent<HTMLInputElement>) {
-    const { name, value } = e.target;
-    setForm((prev) => ({ ...prev, [name]: value }));
-  }
-
-  async function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    try {
-      setSubmitting(true);
-      await login({
-        usernameOrEmail: form.usernameOrEmail,
-        password: form.password,
-      });
-      toast.success("Log in successful!");
-      navigate("/");
-    } catch (err: any) {
-      const message = (err && (err.message || err.detail)) || "Login failed";
-      toast.error(message);
-    } finally {
-      setSubmitting(false);
-    }
-  }
-
   return (
     <>
       <div className="semi-transparent-bg p-3 f-border">
-        <Form onSubmit={handleSubmit}>
+        <form onSubmit={submitLogin}>
           {fields.map((field) => (
             <FormField
               key={field.name}
@@ -64,7 +35,7 @@ export default function LoginForm({ onSwitchToRegister }: LoginFormProps) {
               name={field.name}
               type={field.type}
               placeholder={field.placeholder}
-              value={form[field.name]}
+              value={loginForm[field.name]}
               onChange={setProperty}
             />
           ))}
@@ -77,7 +48,7 @@ export default function LoginForm({ onSwitchToRegister }: LoginFormProps) {
           >
             {submitting ? "Logging in..." : "Log in"}
           </Button>
-        </Form>
+        </form>
 
         <p className="text-center mt-3">
           No account yet?
