dc1f43eab2dfd675a9d18414d330d2895a86ea51
Viktoria <viktoria.pakhrova@outlook.com>
2025-12-08 22:12:59 +0100

    Extracted logic from RegisterForm to useRegisterForm hook instead for cleaner structure

--- frontend/src/feature/auth/RegisterForm.tsx
+++ frontend/src/feature/auth/RegisterForm.tsx
@@ -1,35 +1,21 @@
-import toast from "react-hot-toast";
-import { validateRegister } from "../../../utils/validation";
-import type { RegisterRequest } from "../../../types/auth";
-import { useState } from "react";
-import { useAuth } from "../../../context/AuthProvider";
 import FormField from "../../components/ui/FormField";
 import { Form } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import { useRegisterForm } from "./useRegisterForm";
 
 interface RegisterFormProps {
   onSwitchToLogin: () => void;
 }
 
 export default function RegisterForm({ onSwitchToLogin }: RegisterFormProps) {
-  const [form, setForm] = useState<RegisterRequest>({
-    username: "",
-    email: "",
-    password: "",
-    firstName: "",
-    lastName: "",
-  });
-
-  const { register } = useAuth();
-  const [submitting, setSubmitting] = useState(false);
-
-  const [touched, setTouched] = useState<
-    Partial<Record<keyof RegisterRequest, boolean>>
-  >({});
-
-  const [errors, setErrors] = useState<
-    Partial<Record<keyof RegisterRequest, string>>
-  >({});
+  const {
+    registerForm,
+    errors,
+    touched,
+    submitting,
+    setProperty,
+    submitRegister,
+  } = useRegisterForm();
 
   const fields = [
     {
@@ -64,49 +50,10 @@ export default function RegisterForm({ onSwitchToLogin }: RegisterFormProps) {
     },
   ] as const;
 
-  function setProperty(e: React.ChangeEvent<HTMLInputElement>) {
-    const { name, value } = e.target;
-
-    const updatedForm = { ...form, [name]: value };
-    setForm(updatedForm);
-
-    setTouched((prev) => ({ ...prev, [name]: true }));
-    setErrors(validateRegister(updatedForm));
-  }
-
-  async function handleRegister(event: React.FormEvent) {
-    event.preventDefault();
-
-    const allTouched = Object.fromEntries(
-      Object.keys(form).map((k) => [k, true])
-    ) as Record<keyof RegisterRequest, boolean>;
-
-    setTouched(allTouched);
-
-    const frontendErrors = validateRegister(form);
-    setErrors(frontendErrors);
-    if (Object.keys(frontendErrors).length > 0) {
-      return; // Stop submission if there are frontend errors
-    }
-
-    try {
-      setSubmitting(true);
-      await register(form);
-      toast.success("Account created successfully!");
-    } catch (err: any) {
-      const userErr = err?.errors?.username?.[0];
-      const emailErr = err?.errors?.email?.[0];
-
-      toast.error(userErr || emailErr || "Unable to register account.");
-    } finally {
-      setSubmitting(false);
-    }
-  }
-
   return (
     <>
       <div className="semi-transparent-bg p-3 mb-2 f-border">
-        <Form onSubmit={handleRegister}>
+        <Form onSubmit={submitRegister}>
           {fields.map((field) => (
             <FormField
               key={field.name}
@@ -114,7 +61,7 @@ export default function RegisterForm({ onSwitchToLogin }: RegisterFormProps) {
               name={field.name}
               type={field.type}
               placeholder={field.placeholder}
-              value={form[field.name]}
+              value={registerForm[field.name]}
               onChange={setProperty}
               error={errors[field.name]}
               touched={touched[field.name]}
